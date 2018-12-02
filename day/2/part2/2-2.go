22924a3a2ec2bfa0b9100332b3ef90183cfae4c2
Ben Miller <ben@beepmill.com>
2018-12-01 23:38:25 -0700

    reduce debug output, print answer

--- day/2/part2/2-2.go
+++ day/2/part2/2-2.go
@@ -21,21 +21,21 @@ func main() {
 
 	for i, box := range boxes {
 		for _, candidate := range boxes[i+1:] {
-			var d int
+			var d, dp int
 			for p := 0; p < len(box); p++ {
 				if box[p] != candidate[p] {
 					d++
+					dp = p
 				}
-				/* if d > 1 {
-					log.Printf("%v and %v are not a match (d:%v)", box, candidate, d)
+				if d > 1 {
 					break
-				} */
+				}
 			}
 			if d == 1 {
 				log.Printf("Found a match! %v and %v differ by only one letter!", box, candidate)
+				log.Printf("Answer is %v%v!", box[:dp], box[dp+1:])
 				return
 			}
-			log.Printf("%v and %v are not a match (d:%v)", box, candidate, d)
 		}
 	}
 }
